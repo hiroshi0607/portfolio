10f171fbac25eb652a731b47a972d85f1a891314
hiroshi0607 <okasigennn@gmail.com>
2021-11-17 21:46:00 +0900

    一旦完成

--- js/sample.js
+++ js/sample.js
@@ -77,22 +77,22 @@ function clickBtn4(){
     p5.style.display ="none";
     }
   }
-  function clickBtn5(){
-  const p1 = document.getElementById("pf1");
-  const p2 = document.getElementById("pf2");
-  const p3 = document.getElementById("pf3");
-  const p4 = document.getElementById("pf4");
-  const p5 = document.getElementById("pf5");
+  // function clickBtn5(){
+  // const p1 = document.getElementById("pf1");
+  // const p2 = document.getElementById("pf2");
+  // const p3 = document.getElementById("pf3");
+  // const p4 = document.getElementById("pf4");
+  // const p5 = document.getElementById("pf5");
 
-  if(p5.style.display=="block"){
-    // noneで非表示
-    p5.style.display ="none";
-  }else{
-    // blockで表示
-    p1.style.display ="none";
-    p2.style.display ="none";
-    p3.style.display ="none";
-    p4.style.display ="none";
-    p5.style.display ="block";
-    }
-  }
+  // if(p5.style.display=="block"){
+  //   // noneで非表示
+  //   p5.style.display ="none";
+  // }else{
+  //   // blockで表示
+  //   p1.style.display ="none";
+  //   p2.style.display ="none";
+  //   p3.style.display ="none";
+  //   p4.style.display ="none";
+  //   p5.style.display ="block";
+  //   }
+  // }
